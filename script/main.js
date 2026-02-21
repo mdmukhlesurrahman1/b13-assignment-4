1a6ae2a3a972186285f4b1a0c4e7252d956f6546
mdmukhlesurrahman1 <mdmukhlesurrahman1@gmail.com>
2026-02-22 04:16:20 +0600

    toggle button successfully genareted

---
+++ script/main.js
@@ -0,0 +1,34 @@
+// toggle button
+
+const allBtn = document.getElementById("allBtn");
+const interviewBtn = document.getElementById("InterviewBtn");
+const rejectedBtn = document.getElementById("rejectedBtn");
+const allJobsSection = document.getElementById("all-jobs-section");
+const interviewSection = document.getElementById("interview-section");
+const rejectedSection = document.getElementById("rejected-section");
+
+
+allBtn.addEventListener("click", function () {
+    allBtn.classList.add("btn-active");
+    interviewBtn.classList.remove("btn-active");
+    rejectedBtn.classList.remove("btn-active");
+    allJobsSection.classList.remove("hidden");
+    interviewSection.classList.add("hidden");
+    rejectedSection.classList.add("hidden");
+})
+interviewBtn.addEventListener("click", function () {
+    allBtn.classList.remove("btn-active");
+    interviewBtn.classList.add("btn-active");
+    rejectedBtn.classList.remove("btn-active");
+    allJobsSection.classList.add("hidden");
+    interviewSection.classList.remove("hidden");
+    rejectedSection.classList.add("hidden");
+})
+rejectedBtn.addEventListener("click", function () {
+    allBtn.classList.remove("btn-active");
+    interviewBtn.classList.remove("btn-active");
+    rejectedBtn.classList.add("btn-active");
+    allJobsSection.classList.add("hidden");
+    interviewSection.classList.add("hidden");
+    rejectedSection.classList.remove("hidden");
+})
