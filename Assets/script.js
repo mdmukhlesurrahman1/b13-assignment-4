713f01e7cfc16672aa3e2882afa335857abfa723
mdmukhlesurrahman1 <mdmukhlesurrahman1@gmail.com>
2026-02-27 01:28:51 +0600

    Html Re-organize and Start New Js File

---
+++ Assets/script.js
@@ -0,0 +1,18 @@
+
+
+// Get Toggle Button
+
+const allBtn = document.getElementById("all-btn");
+const interviewBtn = document.getElementById("interview-btn");
+const rejectedBtn = document.getElementById("rejected-btn");
+
+// Get All Section
+
+const interviewSection = document.getElementById("interview-section");
+const rejectedSection = document.getElementById("rejected-section");
+const noJobSection = document.getElementById("no-job-section");
+const allJobsSection = document.getElementById("all-jobs-section");
+
+console.log(allBtn, interviewBtn, rejectedBtn);
+
+
