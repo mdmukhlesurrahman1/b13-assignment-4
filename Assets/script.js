93e1ee387660604b4c841e0d56cbabf6774cec2d
mdmukhlesurrahman1 <mdmukhlesurrahman1@gmail.com>
2026-02-28 04:31:00 +0600

    Bug Fixed

--- Assets/script.js
+++ Assets/script.js
@@ -6,7 +6,6 @@ const rejectedSection = document.getElementById("rejected-section");
 const allJobsSection = document.getElementById("all-jobs-section");
 const noJobSection = document.getElementById("no-job-section");
 
-
 // Switch Tab Create
 function switchTab(tab) {
     const tabs = ["all-btn", "interview-btn", "rejected-btn"]
@@ -50,16 +49,95 @@ document.getElementById("jobs").addEventListener('click', function (event) {
         status.innerText = "INTERVIEWED";
         status.className = ('badge badge-success my-1');
         card.className = ('job-post card bg-base-100 shadow-sm w-full my-4 border-l-4 border-success');
-        interviewSection.appendChild(card);
+        const clone = card.cloneNode(true);
+
+        // Duplicate Check And Add
+        let isDuplicate = false;
+        for (let child of interviewSection.children) {
+            if (child.innerHTML === clone.innerHTML) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate) {
+            interviewSection.appendChild(clone);
+        }
+
+        // Is Available Rejected
+        for (let child of rejectedSection.children) {
+            if (child.querySelector(".company-name").innerText === card.querySelector(".company-name").innerText) {
+                child.remove();
+            }
+        }
+
+        // All Tabs Style Change
+        for (let child of allJobsSection.children) {
+            if (child.querySelector(".company-name").innerText === card.querySelector(".company-name").innerText) {
+                child.className = ('job-post card bg-base-100 shadow-sm w-full my-4 border-l-4 border-success');
+                child.querySelector(".badge").innerText = "INTERVIEWED";
+                child.querySelector(".badge").className = ('badge badge-success my-1');
+            }
+        }
+
     }
+
     else if (clicked.classList.contains('job-rejected-btn')) {
         status.innerText = "REJECTED";
         status.className = ('badge badge-error my-1');
         card.className = ('job-post card bg-base-100 shadow-sm w-full my-4 border-l-4 border-error');
-        rejectedSection.appendChild(card);
+        const clone = card.cloneNode(true);
+
+        // Duplicate Check And Add
+        let isDuplicate = false;
+        for (let child of rejectedSection.children) {
+            if (child.innerHTML === clone.innerHTML) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate) {
+            rejectedSection.appendChild(clone);
+        }
+
+        // Is Available Rejected
+        for (let child of interviewSection.children) {
+            if (child.querySelector(".company-name").innerText === card.querySelector(".company-name").innerText) {
+                child.remove();
+            }
+        }
+
+        // All Tabs Style Change
+        for (let child of allJobsSection.children) {
+            if (child.querySelector(".company-name").innerText === card.querySelector(".company-name").innerText) {
+                child.className = ('job-post card bg-base-100 shadow-sm w-full my-4 border-l-4 border-error');
+                child.querySelector(".badge").innerText = "REJECTED";
+                child.querySelector(".badge").className = ('badge badge-error my-1');
+            }
+        }
     }
+
     else if (clicked.classList.contains('delete-btn')) {
-        parent.removeChild(card);
+        if (currentTab === "all-btn") {
+            parent.removeChild(card);
+            for (let child of interviewSection.children) {
+                if (child.querySelector(".company-name").innerText === card.querySelector(".company-name").innerText) {
+                    child.remove();
+                }
+            }
+            for (let child of rejectedSection.children) {
+                if (child.querySelector(".company-name").innerText === card.querySelector(".company-name").innerText) {
+                    child.remove();
+                }
+            }
+        }
+        else {
+            parent.removeChild(card);
+            for (let child of allJobsSection.children) {
+                if (child.querySelector(".company-name").innerText === card.querySelector(".company-name").innerText) {
+                    child.remove();
+                }
+            }
+        }
     }
     count();
 })
@@ -81,7 +159,13 @@ function count() {
     totalCount.innerText = counts["all-btn"];
     interviewCount.innerText = counts["interview-btn"];
     rejectedCount.innerText = counts["rejected-btn"];
-    jobCount.innerText = counts[currentTab] + ' jobs';
+    if (currentTab === "all-btn") {
+        jobCount.innerText = counts["all-btn"] + ' jobs';
+    }
+    else {
+        jobCount.innerText = counts[currentTab] + ' of ' + counts["all-btn"] + ' jobs';
+    }
+
 
     // NO Job Show Hide Function
     if (counts[currentTab] < 1) {
